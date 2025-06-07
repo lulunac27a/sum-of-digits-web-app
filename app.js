ed9223a3e9f0bf08566bb63b279f7281cc75081e
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-06-07 18:13:21 -0500

    Add radix parameter

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function calculateSum(number) {
     const numberString = number.toString();
     let sum = 0;
     for (let i = 0; i < numberString.length; i++) {
-        const digit = parseInt(numberString[i]);
+        const digit = parseInt(numberString[i], 10);
         sum += digit;
     }
     return sum;
