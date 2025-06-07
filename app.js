de1c33e6a7ca5bf407dea347fa12b9d442dccfa9
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-06-07 09:26:20 -0500

    Fix warning by adding radix parameter to parseInt

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const numberInput = document.getElementById('numberInput');
 const result = document.getElementById('result');
 function calculateSumOfDigits() {
-    const number = parseInt(numberInput.value);
+    const number = parseInt(numberInput.value, 10);
     if (isNaN(number)) {
         result.textContent = 'Please enter a valid number.';
         return;
