2a628971af1d0e9b433ce6e8a8745c45104a3513
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-06-10 20:51:48 -0500

    Fix error caused by non numeric input

--- sum-of-digits/app.js
+++ sum-of-digits/app.js
@@ -6,13 +6,16 @@ function calculateSumOfDigits() {
   const number = parseFloat(numberInput.value);
   if (isNaN(number)) {
     result.textContent = "Please enter a valid number.";
-    return;
   }
   const sum = calculateSum(number);
   result.textContent = `Sum of digits: ${sum}`;
 }
 function calculateSumOfAllDigits(number) {
-  const numberString = number.toString().split("e")[0];
+  const numberString = number
+    .toString()
+    .split("e")[0]
+    .replace(".", "")
+    .replace("-", "");
   let sum = 0;
   for (let i = 0; i < numberString.length; i++) {
     const digit = parseInt(numberString[i], 10);
@@ -22,7 +25,10 @@ function calculateSumOfAllDigits(number) {
 }
 
 function calculateSumOfIntegerDigits(number) {
-  const integerString = parseInt(number, 10).toString();
+  const integerString = parseInt(number, 10)
+    .toString()
+    .replace(".", "")
+    .replace("-", "");
   let sum = 0;
   for (let i = 0; i < integerString.length; i++) {
     const digit = parseInt(integerString[i], 10);
