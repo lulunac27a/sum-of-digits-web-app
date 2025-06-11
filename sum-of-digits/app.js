d96ac90f64ed3594403edb64601b617422b93c5b
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-06-11 15:35:37 -0500

    Add Comments

--- sum-of-digits/app.js
+++ sum-of-digits/app.js
@@ -1,21 +1,24 @@
-const numberInput = document.getElementById("numberInput");
-const result = document.getElementById("result");
-const option = document.getElementById("option");
+const numberInput = document.getElementById("numberInput"); //number input
+const result = document.getElementById("result"); //result display
+const option = document.getElementById("option"); //option selection
 let mode = option.value;
 function calculateSumOfDigits() {
-  const number = parseFloat(numberInput.value);
+  //calculate sum of digits
+  const number = parseFloat(numberInput.value); //get number value from input
   if (isNaN(number)) {
+    //if entered number is not a number
     result.textContent = "Please enter a valid number.";
   }
-  const sum = calculateSum(number);
-  result.textContent = `Sum of digits: ${sum}`;
+  const sum = calculateSum(number); //calculate sum of entered number
+  result.textContent = sum; //display result
 }
 function calculateSumOfAllDigits(number) {
+  //calculate sum of all digits
   const numberString = number
     .toString()
     .split("e")[0]
     .replaceAll(".", "")
-    .replaceAll("-", "");
+    .replaceAll("-", ""); //remove non digit characters to avoid errors
   let sum = 0;
   for (let i = 0; i < numberString.length; i++) {
     const digit = parseInt(numberString[i], 10);
@@ -25,10 +28,11 @@ function calculateSumOfAllDigits(number) {
 }
 
 function calculateSumOfIntegerDigits(number) {
+  //calculate sum of integer digits
   const integerString = parseInt(number, 10)
     .toString()
     .replaceAll(".", "")
-    .replaceAll("-", "");
+    .replaceAll("-", ""); //remove non digit characters to avoid errors
   let sum = 0;
   for (let i = 0; i < integerString.length; i++) {
     const digit = parseInt(integerString[i], 10);
@@ -38,9 +42,12 @@ function calculateSumOfIntegerDigits(number) {
 }
 
 function calculateSum(number) {
+  //calculate sum function
   if (mode === "sumOfDigits") {
-    return calculateSumOfAllDigits(number);
+    //if mode is sum of digits
+    return calculateSumOfAllDigits(number); //return sum of all digits
   } else if (mode === "sumOfIntegerDigits") {
-    return calculateSumOfIntegerDigits(number);
+    //else if mode is sum of integer digits
+    return calculateSumOfIntegerDigits(number); //return sum of integer digits
   }
 }
